6312bd55059259ec04b7b1141abe673303c72cfe
ixguza <rtk0253@gmail.com>
2017-11-21 09:49:37 +0900

    add: ex01_01

---
+++ ch01/ex01/echo.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main() {
+    fmt.Println(echo(os.Args[:]))
+}
+func echo(args []string) string{
+    return strings.Join(args, " ")
+}
+

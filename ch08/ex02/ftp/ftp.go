150bee9966b794004069f637ff01db925ec2c538
cyamaura <c_yamaura@r.recruit.co.jp>
2018-02-26 12:05:49 +0900

    upd ex0802

--- ch08/ex02/ftp/ftp.go
+++ ch08/ex02/ftp/ftp.go
@@ -1,22 +1,31 @@
 package ftp
 
 import (
+    "bufio"
     "fmt"
     "io"
     "net"
+    "net/textproto"
+    "strings"
     //"time"
 )
 
+// var cnt int = 0
 func HandleConn(c net.Conn) {
     defer c.Close()
 
-    fmt.Println("login")
+    // fmt.Println("login")
     handleLogin(c)
-    fmt.Println("login...ok")
+    // fmt.Println("login...ok")
 
     for {
-        readWrite(c)
-        io.WriteString(c, "ok\n")
+        str := readWrite(c)
+        if strings.HasPrefix(str, "PORT") {
+            io.WriteString(c, "200-_PORT\n")
+        } else {
+            io.WriteString(c, fmt.Sprintf("ok. %s\n", str))
+        }
+        // cnt += 1
     }
     /*
     for {
@@ -44,8 +53,26 @@ func handleLogin(c net.Conn) {
     io.WriteString(c, "230\n")
 }
 
-func readWrite(c net.Conn) {
-    buf := make([]byte, 1024)
+var buf []byte = make([]byte, 1024)
+/*
+func readWrite(c net.Conn) string {
     c.Read(buf)
-    fmt.Println(string(buf))
+    str := string(buf)
+    fmt.Print(str)
+    return str
 }
+*/
+func readWrite(c net.Conn) string {
+    r := textproto.NewReader(bufio.NewReader(c))
+    str, _ := r.ReadLine()
+    fmt.Println(str)
+    return str
+}
+
+
+
+
+
+
+
+
